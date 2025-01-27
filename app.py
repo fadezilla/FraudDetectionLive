ffbc71893bee618986283238c1da5a1b1e701094
fadezilla <harleyseierstad@hotmail.com>
2025-01-27 13:35:37 +0100

    Host update

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def predict():
     return jsonify(response)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
